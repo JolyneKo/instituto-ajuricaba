78da52db7dbf3c574c5cca0ea87f4afcee2e918b
JolyneKo <jolynek4444@protonmail.com>
2021-03-20 17:59:11 -0400

    Correções

--- src/project/urls.py
+++ src/project/urls.py
@@ -9,8 +9,10 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from apps.homepage.views import homepage
+from apps.sobre.views import sobre
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', homepage)
+    path('', homepage),
+    path('about', sobre)
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
